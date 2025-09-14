1c5ed2062803d6c51fd876115dc128d2e556a0fc
nathan9513 <nathan9512@proton.me>
2025-09-14 05:30:02 +0200

    Remove unused import of androidx.core.app.NotificationManagerCompat

--- app/src/main/java/android/support/v4/app/NotificationManagerCompat.java
+++ app/src/main/java/android/support/v4/app/NotificationManagerCompat.java
@@ -1,7 +1,5 @@
 package android.support.v4.app;
 
-import androidx.core.app.NotificationManagerCompat;
-
 public final class NotificationManagerCompat {
     private NotificationManagerCompat() {}
     public static java.util.Set<String> getEnabledListenerPackages(android.content.Context ctx) {
